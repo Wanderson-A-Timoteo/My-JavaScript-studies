5f48816bfc54f627b8b11acd033857fa291eb4f4
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2020-12-29 19:14:51 -0400

    add estrutura d repetição forçada, while e do while

---
+++ Curso em Video/Aula13/repeticao.js
@@ -0,0 +1,33 @@
+/** DO... WHILE... Estrutura de repetição que primeiro executa o comando e depois verifica se a condição é verdadeira
+*  para continuar executando. Caso seja falsa ela sai do laço de repetição
+ */
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/**
+ * WHILE.. Estrutura de repetição com condição while que testa primeiro para verificar se a condição é verdadeira
+ *  e depois executa a repetição. Caso seja falsa ela sai do laço de repetição
+ * 
+ * var c = 1
+ * while ( c <= 6 ) {
+ *  console.log(`Passo ${c}`)
+ *  c++
+ * }
+ */
+
+
+ 
+ /**
+  * Estrutura de repetição forçada sem uma condição de teste
+  * 
+  * console.log('Tudo bem?')
+  * console.log('Tudo bem?')
+  * console.log('Tudo bem?')
+  * console.log('Tudo bem?')
+  * console.log('Tudo bem?')
+  * console.log('Tudo bem?')
+  */
