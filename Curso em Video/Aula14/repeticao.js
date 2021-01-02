15e5db4058e1c6186b79570dd7a0833285674e40
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-01-02 18:32:47 -0400

    add aula14 sobre estrutura de repetição FOR

---
+++ Curso em Video/Aula14/repeticao.js
@@ -0,0 +1,16 @@
+/**Exemplo de estrutura de repeticção com while
+ * 
+var c = 1
+while (c <= 10 ){
+    console.log(c)
+    c++
+}
+
+*/
+
+/**Exemplo de estrutura de repeticção com FOR*/
+console.log('Vai começar!')
+for (var c = 1; c <=4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
