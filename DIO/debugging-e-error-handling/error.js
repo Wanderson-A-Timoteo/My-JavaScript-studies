db529c5e51f522409b786ffa82e5845e96b89faa
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-10 09:31:26 -0400

    adicionado try ...catch para cada tipo de erro

--- DIO/debugging-e-error-handling/error.js
+++ DIO/debugging-e-error-handling/error.js
@@ -18,13 +18,31 @@ Filtre as chamadas de catch por cada tipo de erro utilizando o operador instance
 */
 
 function validaArray(arr, num) {
-  if (!arr && !num) throw new ReferenceError("Envie os parâmetros");
+  try {
+    if (!arr && !num) throw new ReferenceError("Envie os parâmetros");
 
-  if (typeof arr !== "object")
-    throw new TypeError("Array precisa ser do tipo object");
+    if (typeof arr !== "object")
+      throw new TypeError("Array precisa ser do tipo object");
 
-  if (typeof num !== "number")
-    throw new TypeError("Array precisa ser do tipo number");
+    if (typeof num !== "number")
+      throw new TypeError("Array precisa ser do tipo number");
 
-  if (arr.length !== num) throw new RangeError("Tamanho inválido");
+    if (arr.length !== num) throw new RangeError("Tamanho inválido");
+
+    // Se array valido retorna o array
+    return arr;
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.log("Este erro é um ReferenceError!");
+      console.log(error.message);
+    } else if (error instanceof TypeError) {
+      console.log("Este erro é um TypeError!");
+      console.log(error.message);
+    } else if (error instanceof RangeError) {
+      console.log("Este erro é um RangeError!");
+      console.log(error.message);
+    } else {
+      console.log("Tipo de erro não esperado: " + error);
+    }
+  }
 }
