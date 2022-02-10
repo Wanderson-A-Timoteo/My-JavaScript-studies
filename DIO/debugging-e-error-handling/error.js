a70751af27eed7c328563cd1f7ee23414f36c69a
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-10 09:23:36 -0400

    Criado validação de erro para cada item solicitado na atividade

---
+++ DIO/debugging-e-error-handling/error.js
@@ -0,0 +1,30 @@
+/*
+
+Tratamento de Erros
+
+Atividade: validação de erros por tipo
+O objetivo é que a função receba um array e retorne ele caso o seu tamanho corresponda ao número enviado como parâmetro na função. Caso contrário, um erro será lançado.
+
+Crie uma função que recebe um array e um número
+Realize as seguintes validações
+ - Se os parâmetros não forem enviados, lance um erro do tipo ReferenceError
+ - Se o array não for do tipo 'object', lance um erro do tipo TypeError
+ - Se o número não for do tipo 'number', lance um erro do tipo TypeError
+ - Se o tamanho do array for diferente do número enviado como parâmetro, lance um erro do tipo RangeError
+Utilize a declaração try...catch
+Filtre as chamadas de catch por cada tipo de erro utilizando o operador instanceof
+
+
+*/
+
+function validaArray(arr, num) {
+  if (!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+  if (typeof arr !== "object")
+    throw new TypeError("Array precisa ser do tipo object");
+
+  if (typeof num !== "number")
+    throw new TypeError("Array precisa ser do tipo number");
+
+  if (arr.length !== num) throw new RangeError("Tamanho inválido");
+}
